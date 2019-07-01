f7c5a736c84283967795d0809c38d9786fb1795d
Ilyas Garipov <ilyasgaripov97@gmail.com>
2019-07-02 01:11:23 +0300

    Refactor illegal garbages list
    Separate labels and content into different containers, small refactor of css

--- src/components/garbage/CurrentGarbageQueries.jsx
+++ src/components/garbage/CurrentGarbageQueries.jsx
@@ -31,47 +31,96 @@ class CurrentGarbageQueries extends React.Component {
                     {/*<div className="date-box"></div>*/}
 
                 </header>
-                
-                <div className="container">
-                    {/* Labels for items*/}
-                    <div className="number-item-label">
-                        <p><u>№</u></p>
-                    </div>
-                    <div className="photo-item-label">
-                        <p><u>Фото</u></p>
-                    </div>
-                    <div className="geoplace-item-label">
-                        <p><u>Местоположение</u></p>
-                    </div>
-                    <div className="comment-item-label">
-                        <p><u>Комментарий</u></p>
-                    </div>
-                    <div className="rate-item-label">
-                        <p><u>Важность</u></p>
-                    </div>
-                    <div className="date-item-label">
-                        <p><u>Дата</u></p>
-                    </div>
-
-                    {/*  Items */}
-                    <div className="number-item">
-                        <p>1</p>
-                    </div>
-                    <div className="photo-item">
+                <div className="containers">
+                    <div className="label-container">
+                        {/* Labels for items*/}
+                        <div className="number-item-label">
+                            <p><u>№</u></p>
                         </div>
-                    <div className="geoplace-item">
+                        <div className="photo-item-label">
+                            <p><u>Фото</u></p>
+                        </div>
+                        <div className="geoplace-item-label">
+                            <p><u>Местоположение</u></p>
+                        </div>
+                        <div className="comment-item-label">
+                            <p><u>Комментарий</u></p>
+                        </div>
+                        <div className="rate-item-label">
+                            <p><u>Важность</u></p>
+                        </div>
+                        <div className="date-item-label">
+                            <p><u>Дата</u></p>
+                        </div>
+                    </div>
+                    <div className="container">
+                        {/*  Items */}
+                        <div className="number-item">
+                            <p>1</p>
+                        </div>
+                        <div className="photo-item">
+                        </div>
+                        <div className="geoplace-item">
+
+                        </div>
+                        <div className="comment-item">
+
+                        </div>
+                        <div className="rate-item">
+
+                        </div>
+                        <div className="date-item">
+
+                        </div>
+
 
                     </div>
-                    <div className="comment-item">
+                    <div className="container">
+                        {/*  Items */}
+                        <div className="number-item">
+                            <p>2</p>
+                        </div>
+                        <div className="photo-item">
+                        </div>
+                        <div className="geoplace-item">
+
+                        </div>
+                        <div className="comment-item">
+
+                        </div>
+                        <div className="rate-item">
+
+                        </div>
+                        <div className="date-item">
+
+                        </div>
+
 
                     </div>
-                    <div className="rate-item">
+                    <div className="container">
+                        {/*  Items */}
+                        <div className="number-item">
+                            <p>3</p>
+                        </div>
+                        <div className="photo-item">
+                        </div>
+                        <div className="geoplace-item">
 
-                    </div>
-                    <div className="date-item">
+                        </div>
+                        <div className="comment-item">
+
+                        </div>
+                        <div className="rate-item">
+
+                        </div>
+                        <div className="date-item">
+
+                        </div>
+
 
                     </div>
                 </div>
+
 
 
             </div>
