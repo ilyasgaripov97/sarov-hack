64e1bd24cf796affe1eebf5c5d217809efc20fa9
Ilyas Garipov <ilyasgaripov97@gmail.com>
2019-07-02 21:16:55 +0300

    Echo data of 'garbage_list' db as json

--- src/backend/test.php
+++ src/backend/test.php
@@ -17,17 +17,30 @@ if ($conn->connect_error) {
 	die("Connection failed: " . $conn->connect_error);
 }
 
-echo "Connected successfully";
+echo "Connected successfully<br>";
 
-$sql = "SELECT id, geo_place FROM garbage_list";
+$sql = "SELECT * FROM garbage_list";
 $output = $conn->query($sql);
 
 if ($output->num_rows > 0) {
 	// output data of each row
 	while($row = $output->fetch_assoc()) {
-		echo "id: " . $row["id"]. " - Geo place: " . $row["geo_place"] . "<br>";
+		$data[] = array(
+			'id' => $row['id'],
+			'img_path' => $row['img_path'],
+			'geo_place' => $row['geo_place'],
+			'comment' => $row['comment'],
+			'importance' => $row['importance'],
+			'date' => $row['date']
+		);
 	}
 } else {
-	echo "0 results";
+	echo "0 results<br>";
 }
+//echo '<pre>'; print_r($output); echo '</pre>';
 $conn->close();
+$output = array('data' => $data);
+$output = json_encode($output);
+
+
+echo $output;
